21dd90e027d3de0c4578065283074a7206e292d3
Issaq <kadavathepeedikayil786786@gmail.com>
2026-01-03 21:30:51 +0530

    more

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
-var arr = [1, 2, 3, 4, 5];
+// array mein kuch bhi dal sakte HTMLDetailsElement, array, number, string, function, object, EventCounts.
+var array = [1, 2, 3, 4, 5, "hey", true, function(){},["one", "two"]];
+var arr = [1,2,3,4,5];
 
+// forEach loop
 arr.forEach(function(val){
     console.log(val + " Hello");
 })
+
+ 
